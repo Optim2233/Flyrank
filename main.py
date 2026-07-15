3df8417705a36a6dc213e28913a0ab65360ae888
Aung Htoo Pyae <sunandmoon8484984@gmail.com>
2026-07-16 01:42:15 +0630

    Stage 1 : Root and Health Endpoints

--- main.py
+++ main.py
@@ -5,3 +5,11 @@ app = FastAPI()
 @app.get("/")
 def root():
     return {"message" : "Hello World"}
+
+@app.get("/tasks")
+def get_tasks():
+    return {"name": "Task API", "version": "1.0", "endpoints": ["/tasks"]}
+
+@app.get("/heath")
+def get_health():
+    return {"status" : "ok"}
