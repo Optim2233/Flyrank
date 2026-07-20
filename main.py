1396131053ff22d43049fee5f6b5668167c92080
Aung Htoo Pyae <sunandmoon8484984@gmail.com>
2026-07-20 23:11:07 +0630

    Extras: <Filtering with query parameters, Search, stats endpoint>

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
-
+from typing import Optional
+import re
 tasks =[
     {"id": 1, "title": "Buy groceries", "done": False},
     {"id": 2, "title": "Finish report", "done": False},
@@ -30,6 +31,10 @@ tags_metadata = [
     {
         "name": "health",
         "description" :"health check"
+    },
+    {
+        "name" : "get stats",
+        "description" : "get statisics of tasks"
     }
 ]
 
@@ -41,10 +46,6 @@ app = FastAPI(openapi_tags=tags_metadata)
 def root():
     return {"message" : "Hello World"}
 
-@app.get("/tasks",tags=["get tasks"])
-def get_tasks():
-    return {"tasks" : tasks}
-
 @app.get("/tasks/{id}",tags=["get task by id"])
 def get_task_by_id(id:int):
     for task in tasks:
@@ -52,6 +53,20 @@ def get_task_by_id(id:int):
             return {"task" : task for task in tasks if task.get("id") == id }
     return {"status" : 404,
             "error" : f"Task {id} not found."}
+    
+
+@app.get("/tasks", tags=["get tasks"])
+def get_done_task(search_term : str = " " ,done: Optional[bool] = None):
+    if done == None and not search_term.strip(" "):
+        return {"tasks" : tasks}
+    elif done == None and search_term.strip(" "):
+        filtered = [task for task in tasks  if re.search(re.escape(search_term), task["title"], re.IGNORECASE)]
+    elif done != None and not search_term.strip(" "):
+        filtered = [task for task in tasks if task.get("done") == done]
+    else : 
+        filtered = [ task for task in tasks if task.get("done") == done and re.search(re.escape(search_term), task["title"], re.IGNORECASE) ]
+    
+    return {"tasks" : filtered}
     
 @app.post("/tasks/", tags=["create task"])
 def post_task(title:str):
@@ -63,16 +78,15 @@ def post_task(title:str):
     return {"status" : 400}
 
 @app.put("/tasks/{id}", tags=["update task"])
-def update_task(id: int, title : str, done : str):
-    if title and title.strip(" ") and done and done.strip(" "):
+def update_task(id: int, title : str, done : bool):
+    if title and title.strip(" ") and done != None:
         for task in tasks:
-            if id == task.get("id"):
+            if task.get("id") == id:
                 task["title"] = title
                 task["done"] = done 
                 return {"status" : 204}
         return {"status" : 404}
     return {"status" : 400}
-        
         
 
 @app.get("/util/get_current_id", tags=["get the pointer location of current id"])
@@ -83,3 +97,17 @@ def _get_current_id():
 @app.get("/heath", tags=["health"])
 def get_health():
     return {"status" : "ok"}
+
+@app.get("/stats", tags=["get stats"])
+def get_stats( ):
+    done_tasks = []
+    open_tasks = []
+    for task in tasks:
+        if task.get("done") == True:
+            done_tasks.append(task)
+        else : 
+            open_tasks.append(task)
+    return {
+        "total" : len(tasks),
+        "done" : len(done_tasks),
+        "open" : len(open_tasks)}
